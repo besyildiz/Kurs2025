915e99a2f4b376a322bde6a75b4e876b5336cd9e
Ekrem <mehterli@gmail.com>
2026-05-14 14:58:00 +0300

    .

--- cizgi_izleyen_4_v2/cizgi_izleyen_4_v2.ino
+++ cizgi_izleyen_4_v2/cizgi_izleyen_4_v2.ino
@@ -11,10 +11,9 @@ unsigned int sensorValues[8];
 #define IN4 9
 
 // --- ADIM AYARLARI ---
-int stepDuration = 80;  // Adımı daha iyi hissetmek için 80ms yaptım
+int stepDuration = 80;  
 int stepWait = 1500;    
 int baseSpeed = 110;    
-// ---------------------
 
 float Kp = 0.05;    
 int previousError = 0;
@@ -28,12 +27,17 @@ void setup() {
   pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
 
-  Serial.println("KALIBRASYON YAPILIYOR...");
-  for (int i = 0; i < 300; i++) {
+  Serial.println("--- KALIBRASYON BASLADI ---");
+  for (int i = 0; i < 200; i++) {
     qtr.calibrate();
-    delay(5);
+    if (i % 20 == 0) {
+      qtr.read(sensorValues);
+      Serial.print("Kalibrasyon... S4 Degeri: "); Serial.println(sensorValues[3]);
+    }
+    delay(10);
   }
-  Serial.println("Poz\tL_Guc\tR_Guc\tDurum");
+  Serial.println("--- HAZIR ---\nPoz\tL_Guc\tR_Guc\tKonum Durumu");
+  delay(1000);
 }
 
 void loop() {
@@ -47,10 +51,13 @@ void loop() {
     }
   }
 
+ // --- HESAPLAMA VE DAVRANIS IYILESDIRME ---
   int error = 3500 - position;
   float P = error;
   float D = error - previousError;
-  int motorSpeed = (Kp * P) + (0.3 * D);
+  
+  // Kp değerini 0.05'ten 0.07'ye çekerek tepkiyi biraz sertleştirdim (Merkeze daha istekli gelir)
+  int motorSpeed = (0.07 * P) + (0.3 * D); 
   previousError = error;
 
   int leftMotorSpeed = 0;
@@ -59,25 +66,38 @@ void loop() {
 
   if (!cizgiVarMi) {
     durum = "CIZGI YOK";
-    leftMotorSpeed = 0;
-    rightMotorSpeed = 0;
+    leftMotorSpeed = 0; rightMotorSpeed = 0;
   } 
-  else if (position >= 2000 && position <= 5000) {
-    durum = "MERKEZDE";
+  else if (position < 1000) {
+    durum = "EN SOLDA";
+    leftMotorSpeed = 150; rightMotorSpeed =  0; 
+  }
+  else if (position >= 1000 && position < 3000) {
+    durum = "MERKEZ SOL";
+    // MERKEZ SOLDA: Sag motorun (right) daha baskın olması lazım
+    // Isaretleri senin motor yonune gore guncelledim:
+    leftMotorSpeed = baseSpeed - motorSpeed; 
+    rightMotorSpeed = baseSpeed + motorSpeed;
+  }
+  else if (position >= 3000 && position <= 4000) {
+    durum = "TAM MERKEZ";
     leftMotorSpeed = baseSpeed - motorSpeed;
     rightMotorSpeed = baseSpeed + motorSpeed;
+  }
+  else if (position > 4000 && position <= 6000) {
+    durum = "MERKEZ SAG";
+    // MERKEZ SAGDA: Sol motorun (left) daha baskın olması lazım
+    leftMotorSpeed = baseSpeed - motorSpeed; 
+    rightMotorSpeed = baseSpeed + motorSpeed;
+  }
+  else {
+    durum = "EN SAGDA";
+    leftMotorSpeed = 0; rightMotorSpeed = 150;
+  }
+  // Hız kısıtlamaları (Sadece çizgi varken ve merkez bölgelerindeyse uygula)
+  if (cizgiVarMi && durum.startsWith("MERKEZ")) {
     leftMotorSpeed = constrain(leftMotorSpeed, 75, 160); 
     rightMotorSpeed = constrain(rightMotorSpeed, 75, 160);
-  } 
-  else if (position < 2000) {
-    durum = "SOL KENAR";
-    leftMotorSpeed = 0;    // Sol dursun
-    rightMotorSpeed = 150; // Sağ dönsün
-  } 
-  else {
-    durum = "SAG KENAR";
-    leftMotorSpeed = 150;  // Sol dönsün
-    rightMotorSpeed = 0;   // Sağ dursun
   }
 
   // Seri Monitör Çıktısı
@@ -86,26 +106,23 @@ void loop() {
   Serial.print(rightMotorSpeed); Serial.print("\t");
   Serial.println(durum);
 
-  // --- HAREKET KOMUTLARI (DÜZELTİLDİ) ---
+  // --- HAREKET UYGULAMA ---
   if (cizgiVarMi) {
-    // Sol Motor Kontrolü
     if (leftMotorSpeed > 0) {
       digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW); analogWrite(ENA, leftMotorSpeed);
     } else {
       digitalWrite(IN1, LOW); digitalWrite(IN2, LOW); analogWrite(ENA, 0);
     }
 
-    // Sağ Motor Kontrolü
     if (rightMotorSpeed > 0) {
       digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW); analogWrite(ENB, rightMotorSpeed);
     } else {
       digitalWrite(IN3, LOW); digitalWrite(IN4, LOW); analogWrite(ENB, 0);
     }
-    
     delay(stepDuration); 
   }
   
-  motorsStop(); // Adım bittiğinde her ikisini de kapat
+  motorsStop(); 
   delay(stepWait); 
 }
 
